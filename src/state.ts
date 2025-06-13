688d0f6fb14d89e39e9e20484012433f58afa84c
ogaken0506 <ogaken0506@gmail.com>
2025-06-13 16:10:42 +0900

    通信中はサイドバーを全て無効化

--- src/state.ts
+++ src/state.ts
@@ -6,6 +6,11 @@ export let communicationCount = 0;
 export let isCommunicating    = false;
 export let isDirectTeamChoice = true;
 
+const categoryRadioButtons = document.getElementsByClassName('category-input');
+const teamSelectElems = document.getElementsByClassName('team-select');
+const groupSelectElem  = document.getElementById('group-select' ) as HTMLSelectElement;
+const shajoSelectElem  = document.getElementById('shajo-select' ) as HTMLSelectElement;
+
 export function setCurrentScoreboard(arg:Scoreboard){
   currentSB = arg;
 }
@@ -15,13 +20,57 @@ export function changeCommunicationState(arg:number){
   communicationCount = communicationCount + arg;
   if(communicationCount > 0){
     isCommunicating = true;
+    disableSidebar();
     registerButton.textContent = "通信中...";
   }else{
     isCommunicating = false;
+    enableSidebar();
     if(registerButton.textContent == "通信中...")registerButton.textContent = "登録";
   }
 }
 
 export function setSelectionMode(arg:boolean){
   isDirectTeamChoice = arg;
+}
+
+function enableSidebar(){
+  for(let i=0; i<categoryRadioButtons.length; i++){
+    categoryRadioButtons[i].removeAttribute('disabled');
+  }
+  let matchSelectElems = document.getElementsByClassName('match-select');
+  for(let i=0; i<matchSelectElems.length; i++){
+    matchSelectElems[i].removeAttribute('disabled');
+  }
+  let commonButtons = document.getElementsByClassName('sidebar-common-button');
+  for(let i=0; i<commonButtons.length; i++){
+    commonButtons[i].removeAttribute('disabled');
+  }
+  if(currentSB.method == "distance"){
+    groupSelectElem.setAttribute('disabled', 'true');
+    shajoSelectElem.setAttribute('disabled', 'true');
+    for(let i=0; i<teamSelectElems.length; i++){
+      teamSelectElems[i].setAttribute('disabled', 'true');
+    }
+  }else if(isDirectTeamChoice){
+    groupSelectElem.setAttribute('disabled', 'true');
+    shajoSelectElem.setAttribute('disabled', 'true');
+  }else{
+    for(let i=0; i<teamSelectElems.length; i++){
+      teamSelectElems[i].setAttribute('disabled', 'true');
+    }
+  }
+}
+
+function disableSidebar(){
+  for(let i=0; i<categoryRadioButtons.length; i++){
+    categoryRadioButtons[i].setAttribute('disabled', 'true');
+  }
+  let matchSelectElems = document.getElementsByClassName('match-select');
+  for(let i=0; i<matchSelectElems.length; i++){
+    matchSelectElems[i].setAttribute('disabled', 'true');
+  }
+  let commonButtons = document.getElementsByClassName('sidebar-common-button');
+  for(let i=0; i<commonButtons.length; i++){
+    commonButtons[i].setAttribute('disabled', 'true');
+  }
 }
