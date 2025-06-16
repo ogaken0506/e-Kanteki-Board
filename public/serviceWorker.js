8cd14380262ee0a9f73de14e686a9a7cc502aec6
ogaken0506 <ogaken0506@gmail.com>
2025-06-16 11:33:43 +0900

    CACHE_NAMEを昨日時点に合わせて変更

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME  = "scoreboard-cache-v20250607";
+var CACHE_NAME  = "scoreboard-cache-v20250615";
 const SERVER_URL = "https://ko-wgaca.f5.si/scoreboard/";
 var urlsToCache = [
     SERVER_URL + "index.html",
